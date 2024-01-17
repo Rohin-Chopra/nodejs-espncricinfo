cb4e497cdc9f930ba07d0457feedb2eb38e9e75e
Rohin Chopra <rohinchopra1212@gmail.com>
2024-01-17 22:57:53 +1100

    feat: Summary class for Match Summaries

---
+++ packages/nodejs-espncricinfo/src/summary.ts
@@ -0,0 +1,39 @@
+import * as cheerio from "cheerio";
+
+export class Summary {
+  private getTextFromChildTag(el: cheerio.Element, tagName: string) {
+    const tag = el.children.find((child) => {
+      return child.type === "tag" && child.name === tagName;
+    }) as cheerio.Element | undefined;
+
+    const textNode = tag?.children[0];
+
+    if (textNode?.type === "text") return textNode.data;
+  }
+
+  public async getSummary() {
+    const response = await fetch(
+      "http://static.cricinfo.com/rss/livescores.xml"
+    );
+
+    const data = await response.text();
+
+    const $ = cheerio.load(data, {
+      xml: true,
+    });
+
+    const summaries = $("item")
+      .map((i, el) => {
+        const link = this.getTextFromChildTag(el, "link");
+
+        return {
+          title: this.getTextFromChildTag(el, "title"),
+          link,
+          description: this.getTextFromChildTag(el, "description"),
+          guid: this.getTextFromChildTag(el, "guid"),
+          matchId: link?.split(".html")[0].split("/")[6],
+        };
+      })
+      .toArray();
+  }
+}
