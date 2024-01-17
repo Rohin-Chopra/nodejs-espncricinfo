f96887bb00df92b33b19769f6ba87f9c34e439ef
Rohin Chopra <rohinchopra1212@gmail.com>
2024-01-17 23:09:34 +1100

    refactor: switched to a function based approach

--- packages/nodejs-espncricinfo/src/summary.ts
+++ packages/nodejs-espncricinfo/src/summary.ts
@@ -1,41 +1,37 @@
 import * as cheerio from "cheerio";
 
-export class Summary {
-  private getTextFromChildTag(el: cheerio.Element, tagName: string) {
-    const tag = el.children.find((child) => {
-      return child.type === "tag" && child.name === tagName;
-    }) as cheerio.Element | undefined;
+function getTextFromChildTag(el: cheerio.Element, tagName: string) {
+  const tag = el.children.find((child) => {
+    return child.type === "tag" && child.name === tagName;
+  }) as cheerio.Element | undefined;
 
-    const textNode = tag?.children[0];
+  const textNode = tag?.children[0];
 
-    if (textNode?.type === "text") return textNode.data;
-  }
+  if (textNode?.type === "text") return textNode.data;
+}
 
-  public async getSummary() {
-    const response = await fetch(
-      "http://static.cricinfo.com/rss/livescores.xml"
-    );
+async function getSummary() {
+  const response = await fetch("http://static.cricinfo.com/rss/livescores.xml");
 
-    const data = await response.text();
+  const data = await response.text();
 
-    const $ = cheerio.load(data, {
-      xml: true,
-    });
+  const $ = cheerio.load(data, {
+    xml: true,
+  });
 
-    const summaries = $("item")
-      .map((i, el) => {
-        const link = this.getTextFromChildTag(el, "link");
+  const summaries = $("item")
+    .map((i, el) => {
+      const link = getTextFromChildTag(el, "link");
 
-        return {
-          title: this.getTextFromChildTag(el, "title"),
-          link,
-          description: this.getTextFromChildTag(el, "description"),
-          guid: this.getTextFromChildTag(el, "guid"),
-          matchId: link?.split(".html")[0].split("/")[6],
-        };
-      })
-      .toArray();
+      return {
+        title: getTextFromChildTag(el, "title"),
+        link,
+        description: getTextFromChildTag(el, "description"),
+        guid: getTextFromChildTag(el, "guid"),
+        matchId: link?.split(".html")[0].split("/")[6],
+      };
+    })
+    .toArray();
 
-    return summaries;
-  }
+  return summaries;
 }
